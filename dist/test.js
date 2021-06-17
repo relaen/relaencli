84f93efdcbcc6d1cc9a7622cc7b885fef520c36d
frank <973478129@qq.com>
2021-06-17 21:13:24 +0800

    调整oracle中schema支持

--- dist/test.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const generator_1 = require("./core/generator");
-generator_1.Generator.gen('config.json');
-//# sourceMappingURL=test.js.map
